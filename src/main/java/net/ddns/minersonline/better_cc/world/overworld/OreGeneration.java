417d9c771027575d976d6889861bb63ee1c48d85
ajh123 <samrosoft36@gmail.com>
2021-09-19 17:16:28 +0100

    Delete useless text package

--- src/main/java/net/ddns/minersonline/better_cc/world/overworld/OreGeneration.java
+++ src/main/java/net/ddns/minersonline/better_cc/world/overworld/OreGeneration.java
@@ -5,7 +5,6 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.Features;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
