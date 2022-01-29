c9325fbf6bba3bc8240082d7662654975faf5a54
ajh123 <ajh123@users.noreply.github.com>
2022-01-29 17:15:53 +0000

    Update docs 2

--- .github/workflows/make-doc.sh
+++ .github/workflows/make-doc.sh
@@ -5,4 +5,4 @@ echo "Uploading docs to $SSH_HOST/$DEST"
 
 sudo apt install -y lftp
 set ssl:verify-certificate no
-lftp -e "set ftp:ssl-allow no; mirror -R $GITHUB_WORKSPACE/docs-build $DEST; quit" -u $SSH_USER,$SSH_KEY $SSH_HOST
+lftp -e "set ftp:ssl-allow no; mirror -R $GITHUB_WORKSPACE/docs-build/* $DEST; quit" -u $SSH_USER,$SSH_KEY $SSH_HOST
