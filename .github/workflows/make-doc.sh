a6bd7555cb3dc0438463771c5f967d269c720530
ajh123 <ajh123@users.noreply.github.com>
2022-01-29 16:34:08 +0000

    Upload docs? fix11

--- .github/workflows/make-doc.sh
+++ .github/workflows/make-doc.sh
@@ -4,4 +4,5 @@ DEST="${GITHUB_REF#refs/*/}/docs"
 echo "Uploading docs to $SSH_HOST/$DEST"
 
 sudo apt install -y lftp
+set ssl:verify-certificate no
 lftp -e "mirror -R $GITHUB_WORKSPACE/docs-build $DEST" -u $SSH_USER,$SSH_KEY $SSH_HOST
