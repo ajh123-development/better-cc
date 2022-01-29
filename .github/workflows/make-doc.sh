b008706c73b8264e280bf6cb0bbe4b3f6f5fcd73
ajh123 <ajh123@users.noreply.github.com>
2022-01-29 16:23:56 +0000

    Upload docs? fix11

--- .github/workflows/make-doc.sh
+++ .github/workflows/make-doc.sh
@@ -4,16 +4,4 @@ DEST="${GITHUB_REF#refs/*/}/docs"
 echo "Uploading docs to $SSH_HOST/$DEST"
 
 sudo apt install -y lftp
-# Setup ssh key
-# mkdir -p "$HOME/.ssh/"
-# echo "$SSH_KEY" > "$HOME/.ssh/key"
-# chmod 600 "$HOME/.ssh/key"
-
-# And upload
-# rsync -avc -e "ssh -i $HOME/.ssh/key -o StrictHostKeyChecking=no -p $SSH_PORT" \
-#       "$GITHUB_WORKSPACE/build/docs/lua/" \
-#       "$SSH_USER@$SSH_HOST:/$DEST"
-lftp -e "mirror -R $GITHUB_WORKSPACE/../build/docs/lua $DEST -u $SSH_USER,$SSH_KEY $SSH_HOST"
-#rsync -avc -e "ssh -i $HOME/.ssh/key -o StrictHostKeyChecking=no -p $SSH_PORT" \
-#      "$GITHUB_WORKSPACE/build/docs/javadoc/" \
-#      "$SSH_USER@$SSH_HOST:/$DEST/javadoc"
+lftp -e "mirror -R $GITHUB_WORKSPACE/docs-build $DEST" -u $SSH_USER,$SSH_KEY $SSH_HOST
