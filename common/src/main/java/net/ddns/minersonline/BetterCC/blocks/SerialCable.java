5333696409faec7fba2371d7174a5ef2f23e1375
ajh123 <ajh123@users.noreply.github.com>
2022-11-07 18:01:08 +0000

    Working Hitboxes

--- common/src/main/java/net/ddns/minersonline/BetterCC/blocks/SerialCable.java
+++ common/src/main/java/net/ddns/minersonline/BetterCC/blocks/SerialCable.java
@@ -3,7 +3,6 @@ package net.ddns.minersonline.BetterCC.blocks;
 import net.ddns.minersonline.BetterCC.setup.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
@@ -20,7 +19,15 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class SerialCable extends CrossCollisionBlock {
 	private final VoxelShape[] occlusionByIndex;
+	private final VoxelShape[] originalCollisionShapeByIndex;
+	private final VoxelShape[] originalShapeByIndex;
 	public static final BooleanProperty VERTICAL = BooleanProperty.create("vertical");
+
+	static float vertical_shape_f = 2.0F;
+	static float vertical_shape_g = 2.0F;
+	static float vertical_shape_h = 16.0F;
+	static float vertical_shape_i = 16.0F;
+	static float vertical_shape_j = 16.0F;
 
 	public SerialCable(Properties properties) {
 		super(2.0F, 2.0F, 3.0F, 3.0F, 3.0F, properties);
@@ -32,7 +39,9 @@ public class SerialCable extends CrossCollisionBlock {
 				.setValue(VERTICAL, false)
 				.setValue(WATERLOGGED, false)
 		);
-		this.occlusionByIndex = this.makeShapes(2.0F, 1.0F, 3.0F, 6.0F, 15.0F);
+		this.occlusionByIndex = this.makeShapes(2.0F, 1.0F, 3.0F, 3.0F, 3.0F);
+		this.originalCollisionShapeByIndex = collisionShapeByIndex.clone();
+		this.originalShapeByIndex = shapeByIndex.clone();
 	}
 
 	public VoxelShape getOcclusionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
@@ -60,20 +69,17 @@ public class SerialCable extends CrossCollisionBlock {
 		BlockPos blockPos4 = blockPos.south();
 		BlockPos blockPos5 = blockPos.west();
 		BlockPos blockPos6 = blockPos.above();
-		BlockPos blockPos7 = blockPos.below();
 		BlockState blockState = blockGetter.getBlockState(blockPos2);
 		BlockState blockState2 = blockGetter.getBlockState(blockPos3);
 		BlockState blockState3 = blockGetter.getBlockState(blockPos4);
 		BlockState blockState4 = blockGetter.getBlockState(blockPos5);
 		BlockState blockState5 = blockGetter.getBlockState(blockPos6);
-		BlockState blockState6 = blockGetter.getBlockState(blockPos7);
 		return super.getStateForPlacement(blockPlaceContext)
 				.setValue(NORTH, this.connectsTo(blockState))
 				.setValue(EAST, this.connectsTo(blockState2))
 				.setValue(SOUTH, this.connectsTo(blockState3))
 				.setValue(WEST, this.connectsTo(blockState4))
 				.setValue(VERTICAL, this.connectsTo(blockState5))
-				.setValue(VERTICAL, this.connectsTo(blockState6))
 				.setValue(WATERLOGGED, fluidState.getType() == Fluids.WATER);
 	}
 
@@ -81,41 +87,58 @@ public class SerialCable extends CrossCollisionBlock {
 		if (blockState.getValue(WATERLOGGED)) {
 			levelAccessor.scheduleTick(blockPos, Fluids.WATER, Fluids.WATER.getTickDelay(levelAccessor));
 		}
-		return updateConnection(blockState, direction);
+		return updateConnection(blockState, direction, blockState2);
+	}
+
+	public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+		VoxelShape[] shape = this.originalShapeByIndex;
+		if (blockState.getValue(VERTICAL)) {
+			shape = this.makeShapes(vertical_shape_f, vertical_shape_g, vertical_shape_h, 0.0F, vertical_shape_i);
+		}
+		return shape[this.getAABBIndex(blockState)];
+	}
+
+	public VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+		VoxelShape[] shape = this.originalCollisionShapeByIndex;
+		if (blockState.getValue(VERTICAL)) {
+			shape = this.makeShapes(vertical_shape_f, vertical_shape_g, vertical_shape_h, 0.0F, vertical_shape_j);
+		}
+
+		return shape[this.getAABBIndex(blockState)];
 	}
 
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		builder.add(NORTH, EAST, WEST, SOUTH, VERTICAL, WATERLOGGED);
 	}
 
-	public BlockState updateConnection(BlockState state, Direction direction){
+	public BlockState updateConnection(BlockState state, Direction direction, BlockState blockState2){
 		if (direction == Direction.NORTH) {
 			state = state.setValue(NORTH, false);
-			if (this.connectsTo(state)) {
+			if (this.connectsTo(blockState2)) {
 				state = state.setValue(NORTH, true);
 			}
 		}
 		if (direction == Direction.EAST) {
 			state = state.setValue(EAST, false);
-			if (this.connectsTo(state)) {
+			if (this.connectsTo(blockState2)) {
 				state = state.setValue(EAST, true);
 			}
 		}
 		if (direction == Direction.SOUTH) {
 			state = state.setValue(SOUTH, false);
-			if (this.connectsTo(state)) {
+			if (this.connectsTo(blockState2)) {
 				state = state.setValue(SOUTH, true);
 			}
 		}
 		if (direction == Direction.WEST) {
 			state = state.setValue(WEST, false);
-			if (this.connectsTo(state)) {
+			if (this.connectsTo(blockState2)) {
 				state = state.setValue(WEST, true);
 			}
 		}
 		if (direction == Direction.UP) {
 			state = state.setValue(VERTICAL, false);
-			if (this.connectsTo(state)) {
+			if (this.connectsTo(blockState2)) {
 				state = state.setValue(VERTICAL, true);
 			}
 		}
